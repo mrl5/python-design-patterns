17cf794cd1a4f5bd0c44465d7945fc6fd7ad1562
mrl5 <jakuboo+github@gmail.com>
2018-09-17 13:46:15 +0200

    new class: Car

---
+++ classes/car.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+class Car():
+    """Product"""
+    def __init__(self):
+        self.model = None
+        self.tires = None
+        self.engine = None
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.model, self.tires, self.engine)
